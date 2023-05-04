066cd0a36b330de856ff38a5bb533a76e0c5b9c3
MrTwinkle777 <101352129+MrTwinkle777@users.noreply.github.com>
2023-05-04 17:51:11 +0300

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,17 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
     use HasFactory;
 
-    public function parent()
+    public function parent(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'parent_id');
     }
 
-    public function children()
+    public function children(): HasMany
     {
         return $this->hasMany(Category::class, 'parent_id');
     }
